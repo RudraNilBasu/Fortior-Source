528ff340ac3a2ee33272a78415f7eca2f2c6ed0f
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-09-16 00:24:20 +0530

    Update end.js

--- end.js
+++ end.js
@@ -12,7 +12,6 @@ function OnTriggerEnter2D(coll : Collider2D)
 	if(coll.tag=="Player")
 	{
 		Instantiate(particles, transform.position, transform.rotation);
-		//particles.SetActive(true);
 		yield WaitForSeconds(2);
 		Application.LoadLevel(Application.loadedLevel+1);
 	}
